c0225511e3d32bbba140a5779745eefdda7efbb7
djblacker2001 <hoangvu222001@gmail.com>
2025-11-01 18:36:43 +0700

    update

--- expressway200.js
+++ expressway200.js
@@ -1,33 +1,14 @@
+// Dữ liệu tuyến đường cao tốc bắc nam phía đông
+const newsData1 = [
 
-//Hiệu ứng hiện trang web khi tải xong
-window.addEventListener("load", () => {
-    document.body.classList.add("loaded");
-});
-
-const navCollapse = document.getElementById('mainNavbar');
-navCollapse.addEventListener('hidden.bs.collapse', () => {
-    iconBars.classList.remove('d-none');
-    iconXmark.classList.add('d-none');
-});
-
-//Hiệu ứng nút go to top
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $(".goTopBtn").fadeIn();
-        } else {
-            $(".goTopBtn").fadeOut();
-        }
-    });
-
-    $(".goTopBtn").click(function() {
-        $("html, body").animate({scrollTop:0}, 0);
-        return false;
-    });
-});
-
-// Dữ liệu tuyến đường cao tốc
-const newsData = [
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Quy_Nh%C6%A1n_%E2%80%93_Ch%C3%AD_Th%E1%BA%A1nh",
+        name: "Đường cao tốc Quy Nhơn – Chí Thạnh",
+        image: "tuyenduong/QuyNhonChiThanh.png",
+        length: "68,32 km",
+        start: "Nút giao Quy Nhơn",
+        end: "Chí Thạnh",
+    },
     {
         link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Ch%C3%AD_Th%E1%BA%A1nh_%E2%80%93_V%C3%A2n_Phong",
         name: "Đường cao tốc Chí Thạnh – Vân Phong",
@@ -108,12 +89,211 @@ const newsData = [
         length: "51,5 km",
         start: "Nút giao Thân Cửu Nghĩa",
         end: "Nút giao An Thái Trung",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_M%E1%BB%B9_Thu%E1%BA%ADn_%E2%80%93_C%E1%BA%A7n_Th%C6%A1",
+        name: "Đường cao tốc Mỹ Thuận – Cần Thơ",
+        image: "tuyenduong/MyThuanCanTho.png",
+        length: "23,6 km",
+        start: "Nút giao Tân Hòa",
+        end: "Nút giao Chà Và",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_C%E1%BA%A7n_Th%C6%A1_%E2%80%93_C%C3%A0_Mau",
+        name: "Đường cao tốc Cần Thơ – Cà Mau",
+        image: "tuyenduong/CanThoCaMau.png",    
+        length: "110.9 km",
+        start: "Nút giao Quốc Lộ 91B",
+        end: "Nút giao Cà Mau",
     }
 ];
 
-const container = document.getElementById("easternID");
-newsData.forEach(news => {
-container.innerHTML += `
+// Dữ liệu tuyến đường cao tốc bắc nam phía tây
+newsData2 = [
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Tuy%C3%AAn_Quang_%E2%80%93_Ph%C3%BA_Th%E1%BB%8D",
+        name: "Đường cao tốc Tuyên Quang – Phú Thọ",
+        image: "tuyenduong/TuyenQuangPhuTho.png",    
+        length: "40,2 km",
+        start: "Nút giao Lưỡng Vượng",
+        end: "Nút giao Phú Thọ",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Gia_Ngh%C4%A9a_%E2%80%93_Ch%C6%A1n_Th%C3%A0nh",
+        name: "Đường cao tốc Gia Nghĩa – Chơn Thành",
+        image: "tuyenduong/GiaNghiaChonThanh.png",    
+        length: "128,8 km",
+        start: "Nút giao Nhân Cơ",
+        end: "Nút giao Chơn Thành",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Ch%C6%A1n_Th%C3%A0nh_%E2%80%93_%C4%90%E1%BB%A9c_H%C3%B2a",
+        name: "Đường cao tốc Chơn Thành – Đức Hòa",
+        image: "tuyenduong/ChonThanhDucHoa.png",    
+        length: "82,75 km",
+        start: "Nút giao Chơn Thành",
+        end: "Nút giao Đức Hòa",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_M%E1%BB%B9_An_%E2%80%93_Cao_L%C3%A3nh",
+        name: "Đường cao tốc Mỹ An – Cao Lãnh",
+        image: "tuyenduong/MyAnCaoLanh.png",    
+        length: "26 km",
+        start: "Nút giao Mỹ An",
+        end: "Nút giao An Bình",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Cao_L%C3%A3nh_%E2%80%93_V%C3%A0m_C%E1%BB%91ng",
+        name: "Đường cao tốc Cao Lãnh – Lộ Tẻ",
+        image: "tuyenduong/CaoLanhVamCong.png",
+        length: "29 km",
+        start: "Nút giao An Bình",
+        end: "Nút giao Lô Tẻ",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_L%E1%BB%99_T%E1%BA%BB_%E2%80%93_R%E1%BA%A1ch_S%E1%BB%8Fi",
+        name: "Đường cao tốc Lộ Tẻ – Rạch Sỏi",
+        image: "tuyenduong/LoTeRachSoi.png",    
+        length: "51 km",
+        start: "Nút giao Lộ Tẻ",
+        end: "Nút giao Rạch Sỏi",
+    },
+
+];
+
+// Dữ liệu một số tuyến đường cao tốc khác
+newsData3 = [
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_H%C3%A0_N%E1%BB%99i_%E2%80%93_H%C3%B2a_B%C3%ACnh_%E2%80%93_S%C6%A1n_La_%E2%80%93_%C4%90i%E1%BB%87n_Bi%C3%AAn",
+        name: "Đường cao tốc Hà Nội – Hòa Bình – Sơn La – Điện Biên",
+        image: "tuyenduong/DaiLoThangLong.png",    
+        length: "445 km",
+        start: "Nút giao Trung Hòa",
+        end: "Nút giao Phiêng Luông",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_H%C3%A0_N%E1%BB%99i_%E2%80%93_H%E1%BA%A3i_Ph%C3%B2ng",
+        name: "Đường cao tốc Hà Nội – Hải Phòng",
+        image: "tuyenduong/HanoiHaiPhong.png",    
+        length: "105,5 km",
+        start: "Nút giao Cổ Linh",
+        end: "Nút giao Đình Vũ",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_N%E1%BB%99i_B%C3%A0i_%E2%80%93_L%C3%A0o_Cai",
+        name: "Đường cao tốc Nội Bài – Lào Cai",
+        image: "tuyenduong/NoiBaiLaoCai.png",    
+        length: "265 km",
+        start: "Nút giao Nội Bài",
+        end: "Nút giao Kim Thành",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_H%E1%BA%A3i_Ph%C3%B2ng_%E2%80%93_H%E1%BA%A1_Long_%E2%80%93_V%C3%A2n_%C4%90%E1%BB%93n_%E2%80%93_M%C3%B3ng_C%C3%A1i",
+        name: "Đường cao tốc Hải Phòng – Móng Cái",
+        image: "tuyenduong/HaiPhongMongCai.png",    
+        length: "176 km",
+        start: "Nút giao Cầu Bạch Đằng",
+        end: "Nút giao Móng Cái",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_H%C3%A0_N%E1%BB%99i_%E2%80%93_Th%C3%A1i_Nguy%C3%AAn_%E2%80%93_B%E1%BA%AFc_K%E1%BA%A1n_%E2%80%93_Cao_B%E1%BA%B1ng",
+        name: "Đường cao tốc Hà Nội – Thái Nguyên",
+        image: "tuyenduong/HanoiThaiNguyen.png",    
+        length: "227 km",
+        start: "Nút giao Ninh Hiệp",
+        end: "Nút giao Bắc Kạn",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Ninh_B%C3%ACnh_%E2%80%93_H%E1%BA%A3i_Ph%C3%B2ng",
+        name: "Đường cao tốc Ninh Bình – Hải Phòng",
+        image: "tuyenduong/NinhBinhHaiPhong.png",    
+        length: "109 km",
+        start: "Nút giao Mai Sơn",
+        end: "Nút giao CT.04",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_N%E1%BB%99i_B%C3%A0i_%E2%80%93_B%E1%BA%AFc_Ninh_%E2%80%93_H%E1%BA%A1_Long",
+        name: "Đường cao tốc Nội Bài – Bắc Ninh – Hạ Long",
+        image: "tuyenduong/NoiBaiBacNinhHaLong.png",    
+        length: "146 km",
+        start: "Nút giao Nội Bài",
+        end: "Nút giao Khả Lễ",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Ti%C3%AAn_Y%C3%AAn_%E2%80%93_L%E1%BA%A1ng_S%C6%A1n_%E2%80%93_Cao_B%E1%BA%B1ng",
+        name: "Đường cao tốc Tiên Yên – Lạng Sơn – Cao Bằng",
+        image: "tuyenduong/TienYenLangSonCaoBang.png",    
+        length: "215 km",
+        start: "Nút giao Tân Thanh",
+        end: "Nút giao Trà Lĩnh",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Y%C3%AAn_B%C3%A1i_%E2%80%93_H%C3%A0_Giang",
+        name: "Đường cao tốc Yên Bái – Hà Giang",
+        image: "tuyenduong/YenBaiHaGiang.png",    
+        length: "81 km",
+        start: "Nút giao Mậu A",
+        end: "Nút giao Việt Vinh",
+    },
+    {
+        link: "https://vi.wikipedia.org/wiki/%C4%90%C6%B0%E1%BB%9Dng_cao_t%E1%BB%91c_Ch%E1%BB%A3_B%E1%BA%BFn_%E2%80%93_Y%C3%AAn_M%E1%BB%B9",
+        name: "Đường cao tốc Chợ Bến – Yên Mỹ",
+        image: "tuyenduong/ChoBenYenMy.png",    
+        length: "60 km",
+        start: "Nút giao Chợ Bến",
+        end: "Nút giao Yên Mỹ",
+    }
+
+];
+
+// Hiển thị dữ liệu tuyến đường cao tốc bắc nam phía đông
+const container1 = document.getElementById("easternID");
+newsData1.forEach(news => {
+container1.innerHTML += `
+    <div class="col-md-3">
+        <a href="${news.link}">
+            <article class="expresswayList">
+                <img src="${news.image}">
+                <div class="container">
+                    <b>${news.name}</b>
+                    <ul>
+                        <li>Chiều dài: ${news.length}</li>
+                        <li>Điểm đầu: ${news.start}</li>
+                        <li>Điểm cuối: ${news.end}</li>
+                    </ul>
+                </div>
+            </article>
+        </a>
+    </div>
+  `;
+});
+
+// Hiển thị dữ liệu tuyến đường cao tốc bắc nam phía tây
+const container2 = document.getElementById("westernID");
+newsData2.forEach(news => {
+container2.innerHTML += `
+    <div class="col-md-3">
+        <a href="${news.link}">
+            <article class="expresswayList">
+                <img src="${news.image}">
+                <div class="container">
+                    <b>${news.name}</b>
+                    <ul>
+                        <li>Chiều dài: ${news.length}</li>
+                        <li>Điểm đầu: ${news.start}</li>
+                        <li>Điểm cuối: ${news.end}</li>
+                    </ul>
+                </div>
+            </article>
+        </a>
+    </div>
+  `;
+});
+
+//Hiển thị dữ liệu một số tuyến đường cao tốc khác
+const container3 = document.getElementById("differentID");
+newsData3.forEach(news => {
+container3.innerHTML += `
     <div class="col-md-3">
         <a href="${news.link}">
             <article class="expresswayList">
@@ -143,5 +323,32 @@ $(document).ready(function () {
         } else {
             $("#mainHeader").removeClass("scrolled");
         }
+    });
+});
+
+//Hiệu ứng hiện trang web khi tải xong
+window.addEventListener("load", () => {
+    document.body.classList.add("loaded");
+});
+
+const navCollapse = document.getElementById('mainNavbar');
+navCollapse.addEventListener('hidden.bs.collapse', () => {
+    iconBars.classList.remove('d-none');
+    iconXmark.classList.add('d-none');
+});
+
+//Hiệu ứng nút go to top
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $(".goTopBtn").fadeIn();
+        } else {
+            $(".goTopBtn").fadeOut();
+        }
+    });
+
+    $(".goTopBtn").click(function() {
+        $("html, body").animate({scrollTop:0}, 0);
+        return false;
     });
 });
